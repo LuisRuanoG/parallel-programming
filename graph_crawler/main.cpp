e05fc9ad56bf1f584cfcb9a3987c1a40a5ca4a73
Luis Angel Ruano Gomez <ruanogomezlu@gmail.com>
2026-02-16 20:34:40 -0800

    Add RapidJSON parsing

--- graph_crawler/main.cpp
+++ graph_crawler/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <curl/curl.h>
+#include <rapidjson/document.h>
+
+using namespace rapidjson;
 
 size_t write_callback(void* contents, size_t size, size_t nmemb, void* userp) {
     size_t total_size = size * nmemb;
@@ -26,6 +30,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // URL-encode the actor name
     char* encoded = curl_easy_escape(curl, actor.c_str(), actor.length());
 
     std::string url =
@@ -33,29 +38,46 @@ int main(int argc, char* argv[]) {
 
     curl_free(encoded);
 
-    std::cout << "Requesting: " << url << std::endl;
-
     std::string response_data;
 
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
     CURLcode res = curl_easy_perform(curl);
 
     if (res != CURLE_OK) {
         std::cerr << "curl failed: " << curl_easy_strerror(res) << "\n";
-    } else {
-        long http_code = 0;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-
-        std::cout << "HTTP code: " << http_code << "\n\n";
-        std::cout << response_data << std::endl;
+        return 1;
     }
 
     curl_easy_cleanup(curl);
     curl_global_cleanup();
+
+
+
+
+    Document doc;
+    doc.Parse(response_data.c_str());
+
+    if (!doc.HasMember("neighbors") || !doc["neighbors"].IsArray()) {
+        std::cerr << "Invalid JSON or no neighbors found.\n";
+        return 1;
+    }
+
+    const Value& neighbors = doc["neighbors"];
+
+    std::vector<std::string> neighbor_list;
+
+    for (SizeType i = 0; i < neighbors.Size(); i++) {
+        neighbor_list.push_back(neighbors[i].GetString());
+    }
+
+    std::cout << "Neighbors of " << actor << ":\n";
+
+    for (const auto& n : neighbor_list) {
+        std::cout << " - " << n << "\n";
+    }
 
     return 0;
 }
