edaa9d7243b3c9c34dabe53d9f7e382551709830
LuisRuanoG <ruanogomezlu@gmail.com>
2026-03-03 21:40:20 -0500

    fix blocking queue deadlock

--- blocking-queue/blocking_queue.h
+++ blocking-queue/blocking_queue.h
@@ -7,39 +7,54 @@
 
 template<typename T>
 class BlockingQueue {
+
 private:
+
     std::queue<T> q;
     std::mutex m;
     std::condition_variable cv;
+
     bool finished = false;
 
 public:
 
     void push(T item) {
+
         std::unique_lock<std::mutex> lock(m);
+
         q.push(item);
+
         cv.notify_one();
     }
 
+
     bool pop(T &item) {
+
         std::unique_lock<std::mutex> lock(m);
 
-        while(q.empty() && !finished)
-            cv.wait(lock);
+        cv.wait(lock, [&]{
+            return !q.empty() || finished;
+        });
 
         if(q.empty())
             return false;
 
         item = q.front();
         q.pop();
+
         return true;
     }
 
-    void set_finished() {
+
+    void set_finished(){
+
         std::unique_lock<std::mutex> lock(m);
+
         finished = true;
+
         cv.notify_all();
     }
+
 };
 
 #endif
